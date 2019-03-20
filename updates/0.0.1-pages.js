1e38e198eb8109e2f4eb7f6680a47547ec5866af
Vladimir Fomene <vladimirfomene@gmail.com>
2019-03-20 14:07:49 +0100

    remove .env file from .gitignore and add seed for pages

---
+++ updates/0.0.1-pages.js
@@ -0,0 +1,5 @@
+exports.create = {
+	Page: [
+		{ 'title': 'Contact', 'state': 'published', 'publishedDate': '2019-03-20', 'image': '', 'content.brief': 'Never Compromise on Identity', 'content.extended': 'We provide a universal authentication & authorization platform for web, mobile and legacy applications.' },
+	],
+};
